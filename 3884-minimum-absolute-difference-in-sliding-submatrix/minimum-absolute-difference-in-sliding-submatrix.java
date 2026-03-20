1f15197eaf9179ef2dbc905a1a3bbf9b7c94d6ed
Meghanareddy06 <165773932+Meghanareddy06@users.noreply.github.com>
2026-03-20 17:52:37 +0530

    Time: 27 ms (42.55%) | Memory: 47.6 MB (53.19%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -11,20 +11,20 @@ class Solution {
 
                 TreeSet<Integer> set = new TreeSet<>();
 
-                // collect elements
+                
                 for (int x = i; x < i + k; x++) {
                     for (int y = j; y < j + k; y++) {
                         set.add(grid[x][y]);
                     }
                 }
 
-                // only one distinct value
+                
                 if (set.size() == 1) {
                     ans[i][j] = 0;
                     continue;
                 }
 
-                // compute min difference
+               
                 int minDiff = Integer.MAX_VALUE;
                 Integer prev = null;
 
